1fdce456d36445f3c6abc273cd110f2efdaef376
Erk-G <erkgarciae@gmail.com>
2024-01-24 12:06:02 -0500

    Fixed NavBar background color

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,7 @@
 import {NavLink} from "react-router-dom";
 const NavBar=()=>{
     return(
-        <div className="text-center min-h-20 bg-[#203647] text-black">
+        <div className="text-center min-h-20 text-white" style={{backgroundColor:"#203647"}}>
             <NavLink to="/">Home</NavLink>
         </div>
     )
