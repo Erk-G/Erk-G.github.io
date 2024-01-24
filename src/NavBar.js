b04afd1069db0a6895d8f335ee76fc7e64509c59
Erk-G <erkgarciae@gmail.com>
2024-01-24 12:35:55 -0500

    Made changes to NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,13 @@
-import {NavLink} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 const NavBar=()=>{
+    const navigate=useNavigate();
+    const btnClass="text-center bg-black-700 hover:bg-black-650 text-white font-bold py-5 px-10 rounded"
+    const navTo=(destination)=>{
+        navigate(`/${destination}`);
+    }
     return(
-        <div className="text-center min-h-20 text-white" style={{backgroundColor:"#203647"}}>
-            <NavLink to="/">Home</NavLink>
+        <div className="grid grid-cols-4 gap-2 py-10" style={{backgroundColor:"#203647"}}>
+            <button className={btnClass} onClick={()=>navTo("")}>Home</button>
         </div>
     )
 }
